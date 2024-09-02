7498733c2a78ce2950d144f30925fac572943c1b
M0o4 <indrei457@gmail.com>
2024-09-02 13:10:23 +0300

    fix: fix app-2 build command

--- app-2/src/app/app.routes.ts
+++ app-2/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SampleComponent } from './components/sample.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '2',
+  },
+  {
+    path: '2',
+    component: SampleComponent,
+  },
+];
---
+++ app-2/src/app/components/sample.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrl: './sample.component.scss',
+  standalone: true,
+})
+export class SampleComponent {}
